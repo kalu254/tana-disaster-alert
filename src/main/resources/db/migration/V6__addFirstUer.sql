9e63c7c790ef4373dc833152e5196eff4387eb69
kalu <kaluarichama@gmail.com>
2021-02-14 15:10:35 +0300

    Adding first User

---
+++ src/main/resources/db/migration/V6__addFirstUer.sql
@@ -0,0 +1,2 @@
+INSERT INTO user_table (id,username,f_name,l_name,email_address,administrative_rank,region_level,region,phone_number,password)
+            VALUES (1,'Aron','Njenga','njenga@gmail.com','Chief','Location','Ngata',0735565726,1234);
