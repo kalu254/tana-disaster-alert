e5746d25b4e50ab3de13ba4c92c6232a77b69ca3
kalu <kaluarichama@gmail.com>
2021-02-15 12:46:31 +0300

    flyway unstable

--- src/main/resources/db/migration/V1__initialScript.sql
+++ src/main/resources/db/migration/V1__initialScript.sql
@@ -1,41 +1,32 @@
-CREATE TABLE IF NOT EXISTS chief_table (
+CREATE TABLE IF NOT EXISTS user_table (
 
-   chief_service_id    varchar(20)
-                      constraint chief_table_pk
-                          PRIMARY KEY,
+   id             serial UNIQUE,
+   username            varchar(20)
+                              constraint user_table_pk
+                                          PRIMARY KEY,
    f_name text not null,
    l_name text not null,
    email_address varchar(50) not null,
-   location text not null,
+   administration_level varchar(50) not null,
+   region_level text not null,
+   region text not null,
    phone_number int not null,
    password varchar(300) not null
 );
 
-CREATE TABLE IF NOT EXISTS commissioner_table (
-
-  commissioner_service_id  varchar(20)
-                      constraint commissioner_table_pk
-                      PRIMARY KEY,
-  f_name text not null,
-  l_name text not null,
-  email_address varchar(50) not null,
-  county text not null,
-  phone_number int not null,
-  password varchar(300) not null
-);
 
 CREATE TABLE IF NOT EXISTS disaster_table (
 
-  disaster_id    serial
+  id        serial UNIQUE
            constraint disaster_table_pk
            PRIMARY KEY,
   disaster_type text not null,
   disaster_description text not null,
   date_reported date not null,
   village text not null,
-  chief_service_id varchar
-          constraint disaster_table_chief_table_id_fk
-              references chief_table,
+  username          varchar
+                    constraint disaster_table_user_table_username_fk
+                    references user_table,
   disaster_img_url_one varchar(100) not null,
   disaster_img_url_two varchar(100),
   disaster_img_url_three varchar(100),
@@ -44,7 +35,7 @@ CREATE TABLE IF NOT EXISTS disaster_table (
 );
 
 CREATE TABLE IF NOT EXISTS affected_unit_table (
-
+   id      serial UNIQUE,
    id_no   int
           constraint affected_unit_table_pk
           PRIMARY KEY,
@@ -54,11 +45,15 @@ CREATE TABLE IF NOT EXISTS affected_unit_table (
    occupants int   not null,
    p_l_w_d int not null,
    children_under_five int not null,
-   chief_service_id varchar
-             constraint affected_unit_table_chief_table_id_fk
-                 references chief_table,
+   username varchar
+             constraint affected_unit_table_user_table_username_fk
+                 references user_table,
    disaster_id int
          constraint affected_unit_table_disaster_table_id_fk
                  references disaster_table
 
 );
+
+INSERT INTO user_table (username,f_name,l_name,email_address,administration_level,region_level,region,phone_number,password)
+    VALUES('kalu','Luka','Macharia','kaluarichama@gmail.com','Chief','Location','Kata Ndogo',746882415,1234);
+
--- src/main/resources/db/migration/V5__addFirstUer.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO user_table (id,username,f_name,l_name,email_address,administrative_rank,region_level,region,phone_number,password)
-            VALUES (1,'kalu','Luka','kaluarichama@gmail.com','Chief','Location','Kada',0735565726,1234);
