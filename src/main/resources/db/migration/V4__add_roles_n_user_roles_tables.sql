f6793379a22bc6844efd50c1b9abbc6f6ed66b2d
Luka Irungu <irungu.luka@eclectics.io>
2024-01-08 00:14:58 +0300

    adjusting datasource configurations

--- src/main/resources/db/migration/V4__add_roles_n_user_roles_tables.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS roles_table (
-   id      SERIAL
-             constraint roles_table_pk
-             PRIMARY KEY,
-   name    varchar(20)
-);
-
--- src/main/resources/db/migration/V4__insert_roles.sql
+++ src/main/resources/db/migration/V4__insert_roles.sql
@@ -1,8 +1,3 @@
-CREATE TABLE IF NOT EXISTS user_roles_table (
-   user_id      bigint,
-   role_id    int
-);
-
 INSERT INTO roles_table(name) VALUES('ADMIN');
 INSERT INTO roles_table(name) VALUES('COMMISSIONER');
 INSERT INTO roles_table(name) VALUES('CHIEF');
